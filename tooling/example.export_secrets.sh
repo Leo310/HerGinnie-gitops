2e00e525f8c467dd7bdb93b359de7371372a94d4
Leo310 <leodev310@gmail.com>
2022-11-05 20:56:26 +0000

    update commit for upto date secrets

--- tooling/example.export_secrets.sh
+++ tooling/example.export_secrets.sh
@@ -6,5 +6,6 @@
 export CF_API_EMAIL=<your_key_email>
 export CF_API_TOKEN=<your_key_id>
 export CF_TUNNEL_CREDS=<tunnel creds>
+export CF_TUNNEL_ORIGIN_CERT=<your cert.pem file content>
 export DO_INLETS_TOKEN=<your digital ocean token>
 export MYSQL_SECRET=<mysql db secret>
